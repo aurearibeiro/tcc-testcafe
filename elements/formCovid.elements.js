47aa7ac86c6e5b9752fb65e62e8db8fda6b7b05c
JhenniferJS <jhennifer@gec.inatel.br>
2022-06-22 18:39:26 -0300

    agrupando elementos

---
+++ elements/formCovid.elements.js
@@ -0,0 +1,17 @@
+export class FormCovidElements {
+  static form = "#ctl00_Corpo_UCModeloAvaliacaoPU1_lblAvaliacao";
+  static answer1 =
+    "#ctl00_Corpo_UCModeloAvaliacaoPU1_GridDados_ctl02_rdbOpcao_0";
+  static answer2 =
+    "#ctl00_Corpo_UCModeloAvaliacaoPU1_GridDados_ctl03_rdbOpcao_1";
+  static answer3 =
+    "#ctl00_Corpo_UCModeloAvaliacaoPU1_GridDados_ctl04_rdbOpcao_4";
+  static answer4 =
+    "#ctl00_Corpo_UCModeloAvaliacaoPU1_GridDados_ctl05_rdbOpcao_1";
+  static answer5 =
+    "#ctl00_Corpo_UCModeloAvaliacaoPU1_GridDados_ctl06_rdbOpcao_1";
+  static answer6 =
+    "#ctl00_Corpo_UCModeloAvaliacaoPU1_GridDados_ctl07_rdbOpcao_2";
+  static buttonConfirm = "#ctl00_Corpo_UCModeloAvaliacaoPU1_btnConfirmar1";
+  static buttonContinue = "#ctl00_Corpo_UCModeloAvaliacaoPU1_Button3";
+}
---
+++ elements/portal.elements.js
@@ -0,0 +1,41 @@
+export class PortalElements {
+  static historic = "#ctl00_Corpo_HyperLink5";
+  static complementaryActivity = {
+    access: "#ctl00_Corpo_HyperLink3",
+    message: "#ctl00_Corpo_UCNotas1_lblErro",
+  };
+  static noteRequest = {
+    access: "#ctl00_Corpo_HyperLink6",
+    message: "#ctl00_Corpo_lblErro",
+  };
+  static orderInquiry = {
+    access: "#ctl00_Corpo_HyperLink13",
+    message: "#ctl00_Corpo_UCConsultaPedidosNotas1_lblErroConsultaPedidos",
+  };
+  static requirements = {
+    access: "#ctl00_Corpo_HyperLink15",
+    requirementOption: "#ctl00_Corpo_dropProcesso",
+  };
+  static intershipGrade = {
+    access: "#ctl00_Corpo_HyperLink14",
+    intershipInitials: "#ctl00_Corpo_UCNotaEstagio1_notaEstagioGridView td",
+    intershipName: "#ctl00_Corpo_UCNotaEstagio1_notaEstagioGridView td",
+  };
+  static requirementsTable = {
+    access: "#ctl00_Corpo_HyperLink28",
+    totalCredits: "tr.SSA_PiscinaLinha td",
+  };
+  static treasury = {
+    access: "#ctl00_Corpo_HyperLink17",
+    tableBankDetails: "#MSO_ContentTable table table table table tr td",
+    tableDebits: "#ctl00_Corpo_UCDebitos1_GridBenner th",
+    bankData: "#ctl00_Corpo_UCDebitos1_Label4",
+    debits: "#ctl00_Corpo_UCDebitos1_lblDebitos",
+    credits: "#ctl00_Corpo_UCDebitos1_lblCreditos",
+  };
+  static testRequests = {
+    access: "#ctl00_Corpo_HyperLink24",
+    course: "#ctl00_Corpo_UCPedidosProvasPresencial1_tvwProvat1",
+  };
+  static grades = "#ctl00_MenuLateral_UCMenuAcademico10_HyperLink5";
+}
